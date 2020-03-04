5b59e557c716a18089ac166640aa197d7cf8b45e
lekhana3003 <lekhanag.3003@gmail.com>
2020-03-05 01:10:07 +0530

    With service discovery

--- discovery-server/src/main/java/com/example/demo/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/demo/DiscoveryServerApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class UserInformationServiceApplication {
+@EnableEurekaServer
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserInformationServiceApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
--- movie-catalogue-service/src/main/java/com/example/demo/MovieCatalogueServiceApplication.java
+++ movie-catalogue-service/src/main/java/com/example/demo/MovieCatalogueServiceApplication.java
@@ -3,15 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MovieCatalogueServiceApplication {
 
+	@LoadBalanced
 	@Bean
-	public WebClient getWebClientBuilder()
-	{
-		return  WebClient.create();
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
